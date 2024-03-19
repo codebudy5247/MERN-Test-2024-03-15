57cb747658c4fe5caa84519dcd994b24b70ac223
codebudy5247 <a.shekhar5247@gmail.com>
2024-03-19 19:09:24 +0530

    fixed issue

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,8 @@
-import { postRouter } from "~/server/api/routers/post";
 import { authRouter } from "./routers/auth";
 import { categoryRouter } from "./routers/category";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   auth:authRouter,
   category:categoryRouter
 });
